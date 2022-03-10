fdc3b979165adbfb7826ab255e26f62aa33b9156
azlathi <azlathi@gmail.com>
2022-03-10 14:41:43 -0700

    modified files

--- baseline.py
+++ baseline.py
@@ -1,3 +1,8 @@
 from datasets import load_dataset
 
-dataset = load_dataset("samsum")
+dataset = load_dataset("billsum", split="ca_test")
+
+billsum = billsum.train_test_split(test_size=.2)
+
+billsum["train"][0]
+
