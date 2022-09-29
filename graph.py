84eea5e1cdf2f41e11ffdc58d49350bd8c1ed21f
marko-kreso <markokreso@gmail.com>
2022-09-28 19:28:12 -0700

    testing vectorizing text rank

--- graph.py
+++ graph.py
@@ -1,25 +1,35 @@
 from typing import List
 from typing import Type
+import numpy as np
 class Graph:
-    nodes:List[Type['Node']] = list()
-    neighbors = dict()
 
     def __init__(self, weighted_adjacency_list):
+        self.nodes:List[Type['Node']] = list()
+        self.neighbors = dict()
+        self.neighbor_weights = np.empty()
+
         self.weighted_list = weighted_adjacency_list
         self.n = weighted_adjacency_list.shape[0]
+        self.scores = np.full((self.n,1),1/self.n,)
 
 
-        for i in range(self.n):
+        for i in range(self.scores.shape[0]):
             self.nodes.append(Node(i, 1/self.n))
 
         for node in self.nodes:
             self.neighbors[node] = [self.nodes[j] for j in range(self.n) if self.edge_weight(node,self.nodes[j]) > 0]
-
         
 
     def get_num_nodes(self):
         return self.n
         
+    def calculate_weights(self):
+        #Calculate sum of neighbors
+        self.neighbor_weights = self.weighted_list @ np.ones(self.weighted_list.shape[0])
+        self.neighbor_weights = (self.weighted_list.T @ np.inverse(np.diag(self.weighted_list))) *  self.scores 
+        
+
+
 
     def get_nodes(self):
         return self.nodes
@@ -35,14 +45,16 @@ class Graph:
         return self.neighbors[node]
 
     def get_scores(self):
-        return [node.score for node in self.nodes] 
+        return self.scores
 
 class Node:
     def __init__(self, index, score):
         self.index = index
         self.score = score
+
     def set_score(self, new_score):
         self.score = new_score 
+        self.scores[self.index] = new_score
     
     def get_index(self):
         return self.get_index
